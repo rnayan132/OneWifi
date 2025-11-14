a999f0b8d79a62faeb035811af8d832adc0debac
rnayan132 <Raghava_Nayani@comcast.com>
2025-11-14 09:36:01 +0000

    RDKB-0 : High impact Coverity Issues Fix
    Reason for change: To fix the Coverity issues which have high impact.
    Test Procedure: Build should be successful and the regression test should also succeed.
    
    Risks: Low
    Priority: P1

--- source/db/wifi_db_apis.c
+++ source/db/wifi_db_apis.c
@@ -8737,6 +8737,7 @@ void get_psm_mac_list_entry(unsigned int instance_number, char *l_vap_name, unsi
             continue;
         }
 
+        memset((void *)temp_psm_mac_param, 0, sizeof(acl_entry_t)); // CID: 330422
         memset(recName, 0, sizeof(recName));
         memset(strValue, 0, sizeof(strValue));
         snprintf(recName, sizeof(recName), MacFilterDevice, instance_number, index);
